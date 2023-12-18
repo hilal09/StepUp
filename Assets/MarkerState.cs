50692f472fd5fb7d5c773bb1f3309ff794a6dae0
hilal09 <hilal.cubukcu02@gmail.com>
2023-12-18 16:45:17 +0100

    neuer ansatz

--- Assets/MarkerState.cs
+++ Assets/MarkerState.cs
@@ -1,22 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProcesPostion : MonoBehaviour
+public class MarkerState : MonoBehaviour
 {
-    [SerializeField] SplineMarker splineMarker;
-    [SerializeField] float scaleX=1920;
- 
     // Start is called before the first frame update
+
+    public bool isActivated=false;
     void Start()
     {
-      
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       splineMarker.SetNormalMarkerPosition(transform.position.x/scaleX);
+        
     }
 }
---
+++ Assets/Scripts/SplineMarker.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplineMarker : MonoBehaviour
+{
+    private SplineAnimate splineAnimate;
+
+    [SerializeField] SplineContainer splineContainer;
+    Spline spline;
+    [SerializeField] GameObject markerPrefab;
+
+    bool isInitialized=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spline=splineContainer.Spline;
+        splineAnimate=transform.GetComponent<SplineAnimate>();
+
+        foreach (BezierKnot bezierKnot in spline.Knots)
+        {
+            //Debug.Log(bezierKnot);
+            GameObject.Instantiate(markerPrefab, new Vector3(bezierKnot.Position.x,bezierKnot.Position.y,bezierKnot.Position.z),bezierKnot.Rotation);
+        }
+    }
+
+    public void SetNormalMarkerPosition(float position)
+    {
+        splineAnimate.NormalizedTime=position;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+        
+        //Switchrenderer(other);
+        PlayAnimation(other);
+
+    }
+
+    private void Switchrenderer(Collider other)
+    {
+        SpriteRenderer renderer=other.transform.GetComponentInChildren<SpriteRenderer>();
+        renderer.enabled=!renderer.enabled;
+    }
+
+    private void PlayAnimation(Collider other)
+    {
+        MarkerState state=other.transform.GetComponentInChildren<MarkerState>();
+        
+         Animator animator=other.transform.GetComponentInChildren<Animator>();
+         if(state.isActivated)
+         {
+            animator.SetTrigger("ScaleOut");
+            state.isActivated=false;
+        }
+        else{
+            animator.SetTrigger("ScaleIn");
+            state.isActivated=true;
+        }
+    }
+
+}
