82f14042374e4b1200242e38c126cd0713b69780
hilal09 <hilal.cubukcu02@gmail.com>
2024-01-10 19:57:04 +0100

    ewrw

--- Assets/Scripts/SplineMarker.cs
+++ Assets/Scripts/SplineMarker.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Splines;
-
+//test
 public class SplineMarker : MonoBehaviour
 {
     private SplineAnimate splineAnimate;
